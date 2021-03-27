81b44727e36e7de24e08276e2e7b750cd1bd74d9
gabin.bloquet <gabin.bloquet@decathlon.com>
2021-03-27 17:42:14 +0100

    fix: problem on get mapping on users endpoint

--- src/main/java/io/gabbloquet/sandbox/User/interfaces/entrypoints/UserController.java
+++ src/main/java/io/gabbloquet/sandbox/User/interfaces/entrypoints/UserController.java
@@ -2,18 +2,15 @@ package io.gabbloquet.sandbox.User.interfaces.entrypoints;
 
 import io.gabbloquet.sandbox.User.domain.entities.User;
 import io.gabbloquet.sandbox.User.domain.useCases.UserService;
-import io.gabbloquet.sandbox.User.domain.useCases.UserServiceImpl;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
 
-import java.net.http.HttpRequest;
 import java.util.List;
 
 @RestController
@@ -21,11 +18,22 @@ import java.util.List;
 @AllArgsConstructor
 public class UserController {
 
+    private static final String RESOURCE_PATH = "{id}";
     private final UserService userService;
 
     @PostMapping()
     public User create(User user) {
         return userService.create(user);
+    }
+
+    @GetMapping()
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    @GetMapping(RESOURCE_PATH)
+    public User get(@PathVariable(required = false) String id) {
+        return null;
     }
 
     @PatchMapping()
@@ -36,15 +44,5 @@ public class UserController {
     @DeleteMapping()
     public void delete(String userId) {
 
-    }
-
-    @GetMapping()
-    public List<User> getAll() {
-        return userService.getAll();
-    }
-
-    @GetMapping()
-    public User get(@RequestParam(required = false) String id) {
-        return null;
     }
 }
